8ddf451abbe05179015610ab3f9f4f44687a8e62
kshitij-vk <koyande72@gmail.com>
2025-05-12 10:37:05 +0530

    Initial commit - Add default python project files chapterwise for first 10 modules.

---
+++ Module 2 - Basics/01 Basic strings and math.py
@@ -0,0 +1,19 @@
+a = 15
+b = 5
+
+# Basic math operations
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+exponentiation = a ** b
+modulus = a % b
+floor_division = a // b
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Exponentiation:", exponentiation)
+print("Modulus:", modulus)
+print("Floor Division:", floor_division)
---
+++ main.py
@@ -0,0 +1,14 @@
+def greet(bot_name, birth_year):
+    print(f"Hello! My name is {bot_name}.")
+    print(f"I was created in {birth_year}.")
+
+def remind_name():
+    print("Please, remind me your name.")
+    # reading a name
+    your_name = input()
+    print(f"What a great name you have, {your_name}!")
+
+
+# Now we can use these functions
+greet("Aid", 2023)
+remind_name()
